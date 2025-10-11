c6fc62657a468372d7ae2a46c20b16d6a64adb19
wolf <wolf@kali.itkhaver>
2025-10-11 06:31:19 -0400

    first commit

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ dotenv.config({
 });
 
 const PORT = process.env.PORT || 5000;
+console.log('CLOUDINARY_CLOUD_NAME', process.env.CLOUDINARY_CLOUD_NAME);
+console.log('CLOUDINARY_API_KEY', process.env.CLOUDINARY_API_KEY);
+console.log('CLOUDINARY_API_SECRET', process.env.CLOUDINARY_API_SECRET);
+
+
 
 connectDB()
 .then(() => {
