839790a8ad1989e8d5df862a99de4a9bf75c5815
wolf <wolf@kali.itkhaver>
2025-10-11 05:08:16 -0400

    first commit

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const allowedOrigins = [
-  'http://localhost:5173',
+  // 'http://localhost:5173',
   'http://sspropertyguru.itkhaver.com',
 ];
 
